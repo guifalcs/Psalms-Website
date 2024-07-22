c17c92499ac246eac4c94b6adea7c4f5b15b621b
Guilherme <guifalcao2017@gmail.com>
2024-07-22 18:07:22 -0300

    Atualizaçao das midias para galeria na sidenavbar

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -22,7 +22,7 @@ const SideNavBar = () => {
             <div className='line'></div>
           <li className='side-navbar-li'><a href='#merch'>Loja</a></li>
             <div className='line'></div>
-          <li className='side-navbar-li'><a href='#media'>Mídias</a></li>
+          <li className='side-navbar-li'><a href='#galeria'>Galeria</a></li>
             <div className='line'></div>
           <li className='side-navbar-li'><a href='#contact'>Contato</a></li>
             <div className='line'></div>

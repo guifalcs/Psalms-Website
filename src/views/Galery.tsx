7de9bc5bba5425dc9dd316fb22522f8470ff459c
Guilherme <guifalcao2017@gmail.com>
2024-07-23 01:04:03 -0300

    Código da galeria melhorado com o chat GPT para esperar as imagens carregarem

--- src/views/Galery.tsx
+++ src/views/Galery.tsx
@@ -4,7 +4,42 @@ import './Galery.scss';
 
 const Galery = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
   const sliderRefs = useRef<(HTMLImageElement | null)[]>([]);
+
+  useEffect(() => {
+    const imageUrls = [
+      "src/assets/img1.jpg",
+      "src/assets/img2.jpg",
+      "src/assets/img3.jpg",
+      "src/assets/img4.jpg",
+      "src/assets/img5.jpg",
+      "src/assets/img6.jpg",
+      "src/assets/img7.jpg",
+      "src/assets/img8.jpg",
+      "src/assets/img9.jpg",
+    ];
+
+    let loadedImagesCount = 0;
+
+    const handleImageLoad = () => {
+      loadedImagesCount++;
+      if (loadedImagesCount === imageUrls.length) {
+        setImagesLoaded(true);
+      }
+    };
+
+    const handleImageError = () => {
+      // Handle image loading errors if needed
+    };
+
+    imageUrls.forEach(url => {
+      const img = new Image();
+      img.src = url;
+      img.onload = handleImageLoad;
+      img.onerror = handleImageError;
+    });
+  }, []);
 
   useEffect(() => {
     showSlider();
@@ -42,72 +77,76 @@ const Galery = () => {
     <>
       <NavBar />
 
-      <div className="galeryContainer">
-        <div className='containerSlider'>
-          <button id='prevButton' onClick={prevSlider}>
-            <img src="src/assets/arrow.png" alt="Previous" />
-          </button>
-          <div className="containerImages">
-            <img
-              className='slider on'
-              src="src/assets/img1.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[0] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img2.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[1] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img3.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[2] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img4.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[3] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img5.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[4] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img6.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[5] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img7.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[6] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img8.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[7] = el)}
-            />
-            <img
-              className='slider'
-              src="src/assets/img9.jpg"
-              alt=""
-              ref={(el) => (sliderRefs.current[8] = el)}
-            />
+      {imagesLoaded ? (
+        <div className="galeryContainer">
+          <div className='containerSlider'>
+            <button id='prevButton' onClick={prevSlider}>
+              <img src="src/assets/arrow.png" alt="Previous" />
+            </button>
+            <div className="containerImages">
+              <img
+                className='slider on'
+                src="src/assets/img1.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[0] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img2.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[1] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img3.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[2] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img4.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[3] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img5.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[4] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img6.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[5] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img7.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[6] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img8.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[7] = el)}
+              />
+              <img
+                className='slider'
+                src="src/assets/img9.jpg"
+                alt=""
+                ref={(el) => (sliderRefs.current[8] = el)}
+              />
+            </div>
+            <button id='nextButton' onClick={nextSlider}>
+              <img src="src/assets/arrow.png" alt="Next" />
+            </button>
           </div>
-          <button id='nextButton' onClick={nextSlider}>
-            <img src="src/assets/arrow.png" alt="Next" />
-          </button>
         </div>
-      </div>
+      ) : (
+        <div>Loading...</div> // Customize this loading message as needed
+      )}
     </>
   );
 };

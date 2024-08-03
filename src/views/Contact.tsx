d95b1431609cdb1f2fd1d18c6dfed1d3c11d2f3d
Guilherme <guifalcao2017@gmail.com>
2024-08-03 13:47:52 -0300

    Blurhash adicionado na contact page

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -1,54 +1,59 @@
-import './Contact.scss'
-import NavBar from '../components/NavBar'
-import { InstagramIcon, LogosYoutubeIcon, CarbonPhoneFilled, IcBaselineEmail } from '../../public/assets/icons'
-import { useEffect, useState } from 'react'
-import Loading from '../components/Loading'
-
+import { useState } from 'react';
+import { Blurhash } from 'react-blurhash';
+import './Contact.scss';
+import NavBar from '../components/NavBar';
+import { InstagramIcon, LogosYoutubeIcon, CarbonPhoneFilled, IcBaselineEmail } from '../../public/assets/icons'; 
 
 const Contact = () => {
 
-  const [imageLoaded, setImageLoaded] = useState(false)
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
-  useEffect(() => {
-    const img = new Image()
-    img.src = './homePhoto.jpg'
-    img.onload = () => {
-      setImageLoaded(true)
-    }
-  }, [])
-  
+  const blurHash = 'L6A0aZoc0Kj[00WC_2ay~UofE1j['; 
+
   return (
     <>
-    {imageLoaded ? <NavBar /> : <Loading/>}
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <NavBar />
+      </div>
 
-                 
+      <div className="contactContainer">
+        {!isImageLoaded && (
+          <Blurhash
+            hash={blurHash}
+            width={window.innerWidth} 
+            height={window.innerHeight} 
+            resolutionX={32} 
+            resolutionY={32} 
+            punch={1} 
+          />
+        )}
 
-        <div className="contactContainer">
+        <img
+          className={`contactImage ${isImageLoaded ? 'loaded' : ''}`}
+          src="/contactPhoto.jpg"
+          alt="Contact"
+          onLoad={() => setIsImageLoaded(true)}
+        />
 
-            <div className='mainBox'>
+        {isImageLoaded && ( <div className='mainBox'>
+          <h1 className='title'>Contato</h1>
 
-                <h1 className='title'>Contato</h1>
-
-               <div className="textBox">
-                    <h3 className='subtitle1'><IcBaselineEmail/><span>Email</span></h3>
-                    <p className='text'>bandapsalms1@gmail.com</p>
-                    <h3 className='subtitle'> <CarbonPhoneFilled/><span>Telefone</span></h3>
-                    <p className='text'>+55 31 7564-7028</p>
-                    
-                  <div className="iconsBelow">
-                    <a className='instaIcon' href="https://www.instagram.com/psalmsband/"><InstagramIcon /></a>
-                    <a className='ytIcon' href="http://www.youtube.com/@psalmsband"><LogosYoutubeIcon /></a>
-                  </div>
-               </div>
-                
-
+          <div className="textBox">
+            <h3 className='subtitle1'><IcBaselineEmail/><span>Email</span></h3>
+            <p className='text'>bandapsalms1@gmail.com</p>
+            <h3 className='subtitle'><CarbonPhoneFilled/><span>Telefone</span></h3>
+            <p className='text'>+55 31 7564-7028</p>
+            
+            <div className="iconsBelow">
+              <a className='instaIcon' href="https://www.instagram.com/psalmsband/"><InstagramIcon /></a>
+              <a className='ytIcon' href="http://www.youtube.com/@psalmsband"><LogosYoutubeIcon /></a>
             </div>
-
-        </div>
-        
-    
+          </div>
+        </div>)}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
+

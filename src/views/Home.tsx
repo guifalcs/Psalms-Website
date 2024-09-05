2570649119bd7addf5b3766cbd2ced82031634f6
Guilherme <guifalcao2017@gmail.com>
2024-09-05 14:19:58 -0300

    correçao pre save para ouça

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -53,7 +53,7 @@ const Home = () => {
       <div className="homeContainer">
         <div className="publicity">
           <img className='capa' src="./capaocaminho.png" alt='Capa "O caminho"' loading='lazy' />
-          <div id='preSave'><a href="https://onerpm.link/PSALMS-TresDias"><button>Pré-save "Três Dias"</button></a></div>
+          <div id='ouca'><a href="https://onerpm.link/PSALMS-TresDias"><button>Ouça "Três Dias"</button></a></div>
           <div className="homeIcons">
             <MdiSpotify/>
             <a className='icon' href="http://www.youtube.com/@psalmsband"><MdiYoutube/></a>

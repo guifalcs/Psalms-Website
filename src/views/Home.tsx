0fac09d7cb734bfb0f8aa81c85ca4ceeb81904be
Guilherme <guifalcao2017@gmail.com>
2024-11-25 11:48:13 -0300

    Reformulação dos conteúdos da home page para colocar os singles do EP "O Caminho"

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,70 +1,90 @@
-import NavBar from '../components/NavBar'
-import './Home.scss'
-import { Blurhash } from 'react-blurhash'
-import { useState, useEffect } from 'react'
-import { MdiSpotify, MdiYoutube, IlInstagram } from '../../public/assets/icons'
-
+import NavBar from "../components/NavBar";
+import "./Home.scss";
+import { Blurhash } from "react-blurhash";
+import { useState, useEffect } from "react";
+import { MdiSpotify, MdiYoutube, IlInstagram } from "../../public/assets/icons";
 
 const Home = () => {
-
-  const [imageLoaded, setImageLoaded] = useState(false)
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
-    const img = new Image()
-    img.src = './homePhoto.jpg'
+    const img = new Image();
+    img.src = "./homePhoto.jpg";
     img.onload = () => {
-      setImageLoaded(true)
-    }
-  }, [])
+      setImageLoaded(true);
+    };
+  }, []);
 
-  const blurHash = 'L48zohWV57xa_Ms:i_fkIVj[aya{'
+  const blurHash = "L48zohWV57xa_Ms:i_fkIVj[aya{";
 
   return (
     <>
-
-      <div style={{ position: 'relative', zIndex: 1 }}>
+      <div style={{ position: "relative", zIndex: 1 }}>
         <NavBar />
       </div>
 
-    <div className='homeImageContainer'>
-      {!imageLoaded && (
+      <div className="homeImageContainer">
+        {!imageLoaded && (
+          <div className="loadingContainer">
+            <Blurhash
+              hash={blurHash}
+              width={window.innerWidth}
+              height={window.innerHeight}
+              resolutionX={32}
+              resolutionY={32}
+              punch={1}
+            />
+          </div>
+        )}
+        <img
+          src="./homePhoto.jpg"
+          alt="Psalms"
+          className={`backgroundImage ${imageLoaded ? "loaded" : "loading"}`}
+          onLoad={() => setImageLoaded(true)}
+        />
 
-        <div className='loadingContainer'>
-        <Blurhash
-          hash={blurHash}
-          width={window.innerWidth}
-          height={window.innerHeight}
-          resolutionX={32}
-          resolutionY={32}
-          punch={1}
-        />
-        </div>
-      )}
-      <img 
-       src="./homePhoto.jpg"
-       alt="Psalms"
-       className={`backgroundImage ${imageLoaded ? 'loaded' : 'loading'}`}
-       onLoad={()=>setImageLoaded(true)}
-        />
-      
-      <h1 id='mainName'>Psalms</h1>
-    </div>
+        <h1 id="mainName">Psalms</h1>
+      </div>
 
       <div className="homeContainer">
         <div className="publicity">
-          <img className='capa' src="./capaocaminho.png" alt='Capa "O caminho"' loading='lazy' />
-          <div id='ouca'><a href="https://onerpm.link/PSALMS-TresDias"><button>Ouça "Três Dias"</button></a></div>
-          <div className="homeIcons">
-            <MdiSpotify/>
-            <a className='icon' href="http://www.youtube.com/@psalmsband"><MdiYoutube/></a>
-            <a className='icon' href="https://www.instagram.com/psalmsband/"><IlInstagram/></a>
+          <div className="single">
+            <div className="singleImgContainer">
+              <img
+                className="singleImg"
+                src="./tbOAV.png"
+                alt=""
+                loading="lazy"
+              />
             </div>
+
+            <h2 className="title">O Amor Venceu</h2>
+
+            <div className="singleContentContainer">
+              <p>
+                Mais do que uma música, "O Amor Venceu" é um testemunho da
+                capacidade do amor divino de superar qualquer desafio e um
+                convite para que todos experimentem e compartilhem essa
+                esperança transformadora.
+              </p>
+            </div>
+
+            <div className="singleLinks">
+              <a href="https://onerpm.link/PSALMS-OAmorVenceu">
+                <MdiSpotify />
+              </a>
+              <a href="https://www.youtube.com/watch?v=FnOGMuY9bDo">
+                <MdiYoutube />
+              </a>
+              <a href="">
+                <IlInstagram />
+              </a>
+            </div>
+          </div>
         </div>
-       </div>
-    
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
